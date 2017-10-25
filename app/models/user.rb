ceafbbb260cf1da30d20964441f7fe7a172f92d6
macro6461 <mattcroak718@gmail.com>
2017-10-25 12:26:10 -0400

    added start of validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :bio, presence: true
+  validates :password, presence: true
+  validates :age, presence: true
+
 end
